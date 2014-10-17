4bd4bfa05ab20ec93232d934d82a27dd0ced5c25
Martin Feckie <mfeckie@gmail.com>
2014-10-17 23:56:20 +1100

    Add index page and navigation

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+    this.route('about');
+    this.resource('lessons', function () {
+        this.route('show', {path: '/:id'});
+        this.route('new');
+        this.route('edit', {path: '/:id/edit'})
+    })
 });
 
 export default Router;
